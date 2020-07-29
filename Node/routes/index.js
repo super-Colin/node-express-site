7c958282c745230a63fe9c9f2977261a26861cd7
superColin <supercolin.dev@gmail.com>
2020-07-28 23:23:02 -0400

    hero section looking decent

--- Node/routes/index.js
+++ Node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Title' });
+  res.render('index', { title: "... It's SuperColin!" });
 });
 
 module.exports = router;
