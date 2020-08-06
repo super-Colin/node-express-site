212c1ea5b3ffee04f3aeeae44eab5c5f2eb84b8f
superColin <supercolin.dev@gmail.com>
2020-08-06 17:26:54 -0400

    deleted lots of unused folders

--- Node/app.js
+++ Node/app.js
@@ -5,10 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
-// D3
-const chartRouter = require('./routes/chart');
 
 const app = express();
 
@@ -23,8 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/chart', chartRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
